df6781ab4c6d8c83f22d330be9670a93134ddade
avatar girase <avatargirase@avatars-MacBook-Pro.local>
2020-05-01 15:48:57 -0700

    Initial commit

---
+++ src/main/java/Firstone.java
@@ -0,0 +1,10 @@
+
+public class Firstone {
+    public static void main(String[] args) {
+        System.err.println("Hey");
+    }
+
+    public void test(){
+        System.err.println("Hi");
+    }
+}
---
+++ src/test/java/FirestoneTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class FirestoneTest {
+    //File->Project Structure->Project
+    //File->Project Structure->Modules
+    //Preferences->Compiler
+    @Test
+    public void test(){
+        System.err.println("hi");
+    }
+
+}
